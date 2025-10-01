abfa4bd482f261443efd13477dae6d3c387d4b60
Random Person <random.code.writer.person@gmail.com>
2025-10-01 19:20:10 +1000

    Don't waste T7/T8 cycles when we build them

--- water/T7.lua
+++ water/T7.lua
@@ -1,11 +1,13 @@
 function RunT7(targetLevel)
     local levels = GetFluidLevels()
-    while levels.t7 < targetLevel do
-        if not PlantControllers.t7.isWorkAllowed() then
-            PlantControllers.t7.setWorkAllowed(true)
-        end
-        os.sleep(120)
-    end
-    PlantControllers.t7.setWorkAllowed(false)
-    return levels.t7 >= targetLevel
+    print("Not yet implemented")
+    return false
+    -- while levels.t7 < targetLevel do
+    --     if not PlantControllers.t7.isWorkAllowed() then
+    --         PlantControllers.t7.setWorkAllowed(true)
+    --     end
+    --     os.sleep(120)
+    -- end
+    -- PlantControllers.t7.setWorkAllowed(false)
+    -- return levels.t7 >= targetLevel
 end
--- water/T8.lua
+++ water/T8.lua
@@ -1,11 +1,13 @@
 function RunT8(targetLevel)
     local levels = GetFluidLevels()
-    while levels.t8 < targetLevel do
-        if not PlantControllers.t8.isWorkAllowed() then
-            PlantControllers.t8.setWorkAllowed(true)
-        end
-        os.sleep(120)
-    end
-    PlantControllers.t8.setWorkAllowed(false)
-    return levels.t8 >= targetLevel
+    print("Not yet implemented")
+    return false
+    -- while levels.t8 < targetLevel do
+    --     if not PlantControllers.t8.isWorkAllowed() then
+    --         PlantControllers.t8.setWorkAllowed(true)
+    --     end
+    --     os.sleep(120)
+    -- end
+    -- PlantControllers.t8.setWorkAllowed(false)
+    -- return levels.t8 >= targetLevel
 end
